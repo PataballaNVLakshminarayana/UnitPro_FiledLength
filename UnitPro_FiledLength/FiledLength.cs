945ff1aeaa2ac58d9938a91a115a70104fa629fa
PataballaNVLakshminarayana <narayana9573944031@gmail.com>
2026-03-02 19:03:46 +0530

    Add project files.

---
+++ UnitPro_FiledLength/FiledLength.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitPro_FiledLength
+{
+    [TestClass]
+    public class FiledLength
+    {
+        private IWebDriver _driver;
+        private By _username = By.Id("username");
+        private By _password = By.Id("password");
+        private By _login = By.Id("login");
+        [TestInitialize]
+        public void DriverInit()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl("https://adactinhotelapp.com/");
+            _driver.Manage().Window.Maximize();
+        }
+        [TestCleanup]
+        public void DriverCleanup()
+        {
+            if(_driver!=null)
+            {
+                _driver.Quit();
+                _driver = null;
+            }
+        }
+        [TestMethod]
+        public void TC_LoginPageFiledLength()
+        {
+            string _user= "nagalakshmin";
+            string _pwd= "l@kshmin";
+            _driver.FindElement(_username).SendKeys(_user);
+            _driver.FindElement(_password).SendKeys(_pwd);
+            _driver.FindElement(_login).Click();
+        }
+    }
+}
---
+++ UnitPro_FiledLength/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitPro_FiledLength")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitPro_FiledLength")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("71d654f8-faf1-45d0-b19b-48a6d7e3fbba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
